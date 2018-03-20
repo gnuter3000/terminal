9ff56063b3e17d51fcf7fe6677d5cb48c30a5ee4
Gunther Weissenbaeck <gunther.weissenbaeck@muellerboon.de>
2018-03-20 16:23:26 +0100

    added tmux

--- terminal.sh
+++ terminal.sh
@@ -5,3 +5,7 @@ rm -fr oh-my-zsh 2>/dev/null
 git clone https://github.com/robbyrussell/oh-my-zsh
 bash ./oh-my-zsh/tools/install.sh
 rm -fr oh-my-zsh 2>/dev/null
+rm -fr .tmux.config 2>/dev/null
+git clone https://github.com/tony/tmux-config
+bash ./tmux-config/install.sh
+rm -fr tmux-config
