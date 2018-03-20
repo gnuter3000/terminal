997e5ff6796cd1de600d4fbc014c8c9303b90740
gnuter3000 <gunther.weissenbaeckl@muellerboon.de>
2018-03-20 16:34:24 +0100

    update

--- terminal.sh
+++ terminal.sh
@@ -1,11 +1,11 @@
 cd ~
+rm -fr .tmux.config 2>/dev/null
+git clone https://github.com/tony/tmux-config
+bash ./tmux-config/install.sh
+rm -fr tmux-config
 rm -fr .zshrc 2>/dev/null
 rm -fr .oh-my-zsh 2>/dev/null
 rm -fr oh-my-zsh 2>/dev/null
 git clone https://github.com/robbyrussell/oh-my-zsh
 bash ./oh-my-zsh/tools/install.sh
 rm -fr oh-my-zsh 2>/dev/null
-rm -fr .tmux.config 2>/dev/null
-git clone https://github.com/tony/tmux-config
-bash ./tmux-config/install.sh
-rm -fr tmux-config
